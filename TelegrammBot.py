b64808702a00c2d73f642bf7d04bc1cf6d369bd9
ELmont19 <ucheba1905@yandex.ru>
2020-02-06 10:33:55 +0300

    Update TelegrammBot.py

--- TelegrammBot.py
+++ TelegrammBot.py
@@ -4,12 +4,12 @@ bot = telebot.TeleBot('986636286:AAGBmIYMoNmH_4VmWdNLHfBE42vi2VStaz4')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-	if message.text == "Hi":
-		bot.send_message(message.from_user.id, "Здарова, уеба")
+	if message.text == "/start":
+		bot.send_message(message.from_user.id, "Здарова, клоун! Я тупой бот, ничего не умею, как и ты)) KEKW")
 
-	elif message.text == "сам уеба" or message.text == "How are u?":
-		bot.send_message(message.from_user.id, "ясно, клоун")
+	elif message.text == "/help" or message.text == "help":
+		bot.send_message(message.from_user.id, "1) /start - приветствие")
 
 	else:
-		bot.send_message(message.from_user.id, "Каво?")
+		bot.send_message(message.from_user.id, "Чаво? Ничего не понял. Напиши /help или help, чтобы узнать команды.")
 bot.polling(none_stop=True, interval=0)
