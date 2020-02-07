cf57269038155e2a3961a058f46d244de8479fb2
Amazing <therzmaxim@gmail.com>
2020-02-07 14:45:23 +0300

    Update TelegrammBot.py

--- TelegrammBot.py
+++ TelegrammBot.py
@@ -11,7 +11,6 @@ def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text='Здарова, клоун! Я тупой бот, ничего не умею, как и ты)) KEKW')
 
 start_handler = CommandHandler('start', start)
-restart_handler=CommandHandler('restart', restart)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(restart_handler)
